eacef36731d2d635f8172e8e276f4897d27fa6f5
Justin Brooks <jsbroks@gmail.com>
2021-05-11 10:34:32 -0400

    fix: stripe env

--- packages/website/src/stripe.tsx
+++ packages/website/src/stripe.tsx
@@ -2,17 +2,13 @@ import React from 'react'
 import { loadStripe, Stripe } from '@stripe/stripe-js'
 import { Elements } from '@stripe/react-stripe-js'
 
-const STRIPE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+const STRIPE_KEY = process.env.NEXT_PUBLIC_STRIPE_KEY
 
 let stripePromise: Promise<Stripe | null>
 
 export const getStripe = () => {
-  if (STRIPE_KEY == null) {
-    throw new Error('No stripe key provided.')
-  }
-  if (stripePromise == null) {
-    stripePromise = loadStripe(STRIPE_KEY)
-  }
+  if (STRIPE_KEY == null) return null
+  if (stripePromise == null) stripePromise = loadStripe(STRIPE_KEY)
   return stripePromise
 }
 
